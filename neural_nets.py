8e32adf527a5a2bce72f751b67c364b49dfa4b67
Nicholas Clarkin-wright <nicholas.clarkin-wright@outlook.com>
2019-12-05 11:20:01 -0500

    ready for hand-in
    Ready to be handed in, mostly above 90% about 70% of the time.

--- neural_nets.py
+++ neural_nets.py
@@ -8,28 +8,29 @@ from keras.layers import Dense
 def get_model():
     # load wine dataset
     dataset = loadtxt('wine.csv', delimiter = ',')
-    X = dataset[:, 1:]
-    y = dataset[:, 0]
+    X = dataset[:,1:]
+    y = dataset[:,0]
 
     # define the keras model
     model = Sequential()
-    model.add(Dense(12, input_dim=13, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(21, input_dim=13, activation='relu'))
+    model.add(Dense(17, activation='relu'))
+    model.add(Dense(13, activation='relu'))
+    model.add(Dense(4, activation='softmax'))
 
     # compile the keras model
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # fit the keras model on the dataset
-    model.fit(X, y, epochs=150, batch_size=10)
+    model.fit(X, y, epochs=200, batch_size=10)
 
     return model
 
-model = get_model()
-
+# Below used for testing
+# model = get_model()
 # evaluate the keras model
-dataset = loadtxt('wine.csv', delimiter = ',')
-X = dataset[:, 1:]
-y = dataset[:, 0]
-_, accuracy = model.evaluate(X, y)
-print('Accuracy: %.2f' %(accuracy*100))
+# dataset = loadtxt('wine.csv', delimiter = ',')
+# X = dataset[:, 1:]
+# y = dataset[:, 0]
+# _, accuracy = model.evaluate(X, y)
+# print('Accuracy: %.2f' %(accuracy*100))
